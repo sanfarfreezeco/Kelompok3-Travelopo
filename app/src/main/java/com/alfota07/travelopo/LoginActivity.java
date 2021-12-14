359a1a44f2886de90963cfa463f93431838c9ae6
sanfarfreezeco <farizcocoa@live.com>
2021-12-14 15:52:51 +0700

    fariz

--- app/src/main/java/com/alfota07/travelopo/LoginActivity.java
+++ app/src/main/java/com/alfota07/travelopo/LoginActivity.java
@@ -3,11 +3,10 @@ package com.alfota07.travelopo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.content.Intent;
 import android.os.Build;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +15,5 @@ public class LoginActivity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
         setContentView(R.layout.activity_login);
-    }
-
-    public void onLoginClick(View View){
-        startActivity(new Intent(this,RegisterActivity.class));
-        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
-
     }
 }
